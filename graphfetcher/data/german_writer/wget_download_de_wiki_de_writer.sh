ae83c28019b6ac6e7a261b7d6792dd32b9fdbf5b
yamauchi.hitoshi <yamauchi@sunday.researcher>
2012-06-01 00:01:44 +0200

    Do not recurse level 2, the Wiki has a lot of links in a page and recurse level 2 fetches quite a lot of irrelevant pages.

--- graphfetcher/data/german_writer/wget_download_de_wiki_de_writer.sh
+++ graphfetcher/data/german_writer/wget_download_de_wiki_de_writer.sh
@@ -13,8 +13,33 @@
 # characters are escaped with %hex values. So I need to set nocontrol
 # and export LC_ALL=en_US.utf-8 to see the files.
 #
-# German metapage also has only the Familyname list, so the recursion
-# level is 2 here.
+# I first set recursion level 2, but then so many unrelated pages are
+# downloaded, therefore I specify all the list pages. There is no X
+# and Y pages.
 #
 #
-wget --restrict-file-names=nocontrol --limit-rate=100K -e 'robots=off' --random-wait --wait=15 -r --level=2 -k -p http://de.wikipedia.org/wiki/Liste_deutschsprachiger_Schriftsteller
+wget --restrict-file-names=nocontrol --limit-rate=100K -e 'robots=off' --random-wait --wait=15 -r --level=1 -k -p \
+    http://de.wikipedia.org/wiki/Liste_deutschsprachiger_Schriftsteller/A	\
+    http://de.wikipedia.org/wiki/Liste_deutschsprachiger_Schriftsteller/B	\
+    http://de.wikipedia.org/wiki/Liste_deutschsprachiger_Schriftsteller/C	\
+    http://de.wikipedia.org/wiki/Liste_deutschsprachiger_Schriftsteller/D	\
+    http://de.wikipedia.org/wiki/Liste_deutschsprachiger_Schriftsteller/E	\
+    http://de.wikipedia.org/wiki/Liste_deutschsprachiger_Schriftsteller/F	\
+    http://de.wikipedia.org/wiki/Liste_deutschsprachiger_Schriftsteller/G	\
+    http://de.wikipedia.org/wiki/Liste_deutschsprachiger_Schriftsteller/H	\
+    http://de.wikipedia.org/wiki/Liste_deutschsprachiger_Schriftsteller/I	\
+    http://de.wikipedia.org/wiki/Liste_deutschsprachiger_Schriftsteller/J	\
+    http://de.wikipedia.org/wiki/Liste_deutschsprachiger_Schriftsteller/K	\
+    http://de.wikipedia.org/wiki/Liste_deutschsprachiger_Schriftsteller/L	\
+    http://de.wikipedia.org/wiki/Liste_deutschsprachiger_Schriftsteller/M	\
+    http://de.wikipedia.org/wiki/Liste_deutschsprachiger_Schriftsteller/N	\
+    http://de.wikipedia.org/wiki/Liste_deutschsprachiger_Schriftsteller/O	\
+    http://de.wikipedia.org/wiki/Liste_deutschsprachiger_Schriftsteller/P	\
+    http://de.wikipedia.org/wiki/Liste_deutschsprachiger_Schriftsteller/Q	\
+    http://de.wikipedia.org/wiki/Liste_deutschsprachiger_Schriftsteller/R	\
+    http://de.wikipedia.org/wiki/Liste_deutschsprachiger_Schriftsteller/S	\
+    http://de.wikipedia.org/wiki/Liste_deutschsprachiger_Schriftsteller/T	\
+    http://de.wikipedia.org/wiki/Liste_deutschsprachiger_Schriftsteller/U	\
+    http://de.wikipedia.org/wiki/Liste_deutschsprachiger_Schriftsteller/V	\
+    http://de.wikipedia.org/wiki/Liste_deutschsprachiger_Schriftsteller/W	\
+    http://de.wikipedia.org/wiki/Liste_deutschsprachiger_Schriftsteller/Z
--- graphfetcher/data/japanese_writer/wget_download_en_wiki_ja_writer.sh
+++ graphfetcher/data/japanese_writer/wget_download_en_wiki_ja_writer.sh
@@ -13,7 +13,33 @@
 # characters are escaped with %hex values. So I need to set nocontrol
 # and export LC_ALL=en_US.utf-8 to see the files.
 #
-# The recursion level is 2, since the meta page only have each
-# familyname meta page.
+# I first set recursion level 2, but then so many unrelated pages are
+# downloaded, therefore I specify all the list pages.
 #
-wget --restrict-file-names=nocontrol --limit-rate=100K -e 'robots=off' --random-wait --wait=15 -r --level=2 -k -p http://en.wikipedia.org/wiki/Japanese_writers
+wget --restrict-file-names=nocontrol --limit-rate=100K -e 'robots=off' --random-wait --wait=15 -r --level=1 -k -p \
+    http://en.wikipedia.org/wiki/List_of_Japanese_authors:A	\
+    http://en.wikipedia.org/wiki/List_of_Japanese_authors:B	\
+    http://en.wikipedia.org/wiki/List_of_Japanese_authors:C	\
+    http://en.wikipedia.org/wiki/List_of_Japanese_authors:D	\
+    http://en.wikipedia.org/wiki/List_of_Japanese_authors:E	\
+    http://en.wikipedia.org/wiki/List_of_Japanese_authors:F	\
+    http://en.wikipedia.org/wiki/List_of_Japanese_authors:G	\
+    http://en.wikipedia.org/wiki/List_of_Japanese_authors:H	\
+    http://en.wikipedia.org/wiki/List_of_Japanese_authors:I	\
+    http://en.wikipedia.org/wiki/List_of_Japanese_authors:J	\
+    http://en.wikipedia.org/wiki/List_of_Japanese_authors:K	\
+    http://en.wikipedia.org/wiki/List_of_Japanese_authors:L	\
+    http://en.wikipedia.org/wiki/List_of_Japanese_authors:M	\
+    http://en.wikipedia.org/wiki/List_of_Japanese_authors:N	\
+    http://en.wikipedia.org/wiki/List_of_Japanese_authors:O	\
+    http://en.wikipedia.org/wiki/List_of_Japanese_authors:P	\
+    http://en.wikipedia.org/wiki/List_of_Japanese_authors:Q	\
+    http://en.wikipedia.org/wiki/List_of_Japanese_authors:R	\
+    http://en.wikipedia.org/wiki/List_of_Japanese_authors:S	\
+    http://en.wikipedia.org/wiki/List_of_Japanese_authors:T	\
+    http://en.wikipedia.org/wiki/List_of_Japanese_authors:U	\
+    http://en.wikipedia.org/wiki/List_of_Japanese_authors:V	\
+    http://en.wikipedia.org/wiki/List_of_Japanese_authors:W	\
+    http://en.wikipedia.org/wiki/List_of_Japanese_authors:X	\
+    http://en.wikipedia.org/wiki/List_of_Japanese_authors:Y	\
+    http://en.wikipedia.org/wiki/List_of_Japanese_authors:Z
