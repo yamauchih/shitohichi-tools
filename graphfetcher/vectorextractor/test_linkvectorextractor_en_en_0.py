fdf8339c08b431a007248e29545a5a00c613d9a3
yamauchi.hitoshi <yamauchi@sunday.researcher>
2012-08-02 02:54:45 +0200

    Implemented English author English Wikipedia link extractor and test.

---
+++ graphfetcher/vectorextractor/test_linkvectorextractor_en_en_0.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2012 Yamauchi, Hitoshi
+#
+# LinkVectorExtractor test
+#
+"""
+\file
+\brief LinkVectorExtractor test
+"""
+
+import os
+import LinkVectorExtractorEnEn
+import unittest
+
+class TestLinkVectorExtractor(unittest.TestCase):
+    """test: LinkVectorExtractor test."""
+
+    def test_linkvectorextractor(self):
+        """test generate URL list by List of English writers (en_en)."""
+        # print u'# Need LC_ALL setting to utf-8, e.g., en_US.utf-8, ja_JP.utf-8.'
+        graphfetcherdir = u'/home/hitoshi/data/project/shitohichi-tools/graphfetcher/'
+
+        input_rpath       = u'data/english_writer/en.wikipedia.org/wiki/'
+        author_root_fname = u'List_of_English_writers'
+
+        indir = os.path.join(graphfetcherdir, input_rpath)
+        input_fullpath = os.path.join(indir, author_root_fname)
+        root_url    = u'file:///' + input_fullpath
+
+        output_rpath      = u'data/english_writer/en.wikipedia.org/wiki/'
+        output_list_fname = u'en_en_writer.vector'
+        outdir = os.path.join(graphfetcherdir, output_rpath)
+        output_full_path = os.path.join(outdir, output_list_fname)
+
+        print u'# input [' + root_url    + u']'
+        print u'# output[' + output_full_path + u']'
+        ignore_href_list = [
+            '../w/index.php', 'Category', '/wiki', 'Wikipedia:', 'Portal',
+            "List_of_children's_literature_authors",
+            'List_of_English_novelists',
+            'List_of_English_writers',
+            'Lists_of_writers',
+            'Special:Random',
+            'Special:RecentChanges',
+            'Special:RecentChangesLinked/List_of_English_writers',
+            'Special:SpecialPages',
+            'Special:WhatLinksHere/List_of_English_writers',
+            'Talk:List_of_English_writers',
+            '/w/index.php?title=List_of_English_writers&oldid=494209347',
+            '/w/index.php?title=Special:Book&bookcmd=book_creator&referer=List+of+English+writers',
+            '/w/index.php?title=Special:Cite&page=List_of_English_writers&id=494209347'
+            ]
+
+        lf = LinkVectorExtractorEnEn.LinkVectorExtractorEnEn(ignore_href_list)
+        lf.get_link_list(root_url, output_full_path)
+
+
+#
+# main test
+#
+if __name__ == '__main__':
+    suit0   = unittest.TestLoader().loadTestsFromTestCase(TestLinkVectorExtractor)
+    alltest = unittest.TestSuite([suit0])
+    unittest.TextTestRunner(verbosity=2).run(alltest)
+
+
--- graphfetcher/vectorextractor/test_linkvectorextractor_en_en_1.py
+++ graphfetcher/vectorextractor/test_linkvectorextractor_en_en_1.py
@@ -3,7 +3,7 @@
 #
 # Copyright (C) 2012 Yamauchi, Hitoshi
 #
-# LinkVectorExtractor test
+# LinkVectorExtractorEnEn test with a small dataset
 #
 """
 \file
@@ -11,31 +11,36 @@
 """
 
 import os
-import LinkVectorExtractor
+import LinkVectorExtractorEnEn
 import unittest
 
 class TestLinkVectorExtractor(unittest.TestCase):
     """test: LinkVectorExtractor test."""
 
     def test_linkvectorextractor(self):
-        """test 日本の小説家一覧を起点にした URL list を生成する．"""
+        """test generate URL list by List of English writers (en_en)."""
         # print u'# Need LC_ALL setting to utf-8, e.g., en_US.utf-8, ja_JP.utf-8.'
         graphfetcherdir = u'/home/hitoshi/data/project/shitohichi-tools/graphfetcher/'
 
-        input_rpath       = u'data/japanese_writer/ja.wikipedia.org/wiki/'
-        author_root_fname = u'日本の小説家一覧'
+        # input_rpath       = u'data/english_writer/en.wikipedia.org/wiki/'
+        # author_root_fname = u'List_of_English_writers'
+        input_rpath       = u'vectorextractor/'
+        author_root_fname = u'en_en_testdata_0.html'
+
         indir = os.path.join(graphfetcherdir, input_rpath)
         input_fullpath = os.path.join(indir, author_root_fname)
         root_url    = u'file:///' + input_fullpath
 
-        output_rpath      = u'data/japanese_writer/ja.wikipedia.org/wiki/'
-        output_list_fname = u'nihonno_shousetuka_ichiran.vector'
+        output_rpath      = u'data/english_writer/en.wikipedia.org/wiki/'
+        output_list_fname = u'en_en_writer.vector'
         outdir = os.path.join(graphfetcherdir, output_rpath)
         output_full_path = os.path.join(outdir, output_list_fname)
 
         print u'# input [' + root_url    + u']'
         print u'# output[' + output_full_path + u']'
-        lf = LinkVectorExtractor.LinkVectorExtractor()
+        ignore_href_list = [
+            '../w/index.php', 'Category', '/wiki', 'Wikipedia:', 'Portal'  ]
+        lf = LinkVectorExtractorEnEn.LinkVectorExtractorEnEn(ignore_href_list)
         lf.get_link_list(root_url, output_full_path)
 
 
