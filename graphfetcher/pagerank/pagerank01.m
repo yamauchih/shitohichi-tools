c8d9f0fa005e1d5d555781ac43e157a2ee4c0a72
hyamauchi <hyamauchi@hyamauchi-lt2.nvidia.com>
2012-08-23 00:47:03 +0200

    Fixed choosing eigenvalue from max(abs(L)) to (max(abs(real(L)))). The norm of complex eigenvalues can be the maximal one. However, we are looking for the real eigenvalue (actually looking for eigenvalue == 1, though can not compare with 1 because of numerical error).

--- graphfetcher/pagerank/pagerank01.m
+++ graphfetcher/pagerank/pagerank01.m
@@ -23,13 +23,20 @@ M_markov = M_adj ./ (ones(nr,1) * sum(M_adj));
 % check it is Markov matrix
 % sum(M_markov)
 
+% check the rank
+rank_m = rank(full(M_markov));
+if (rank_m < nr)
+    fprintf(1, 'warn: M_adj is not full rank (%d of %d)\n', rank_m, nr);
+end
+
 % get eigenvalues and eigen vectors
 [V, Lambda] = eig(M_markov);
 
 % get max absolute value's index of L (get the first value max index)
-[m, idx] = max(diag(abs(Lambda)));
+% [m, idx] = max(diag(abs(Lambda)));
+[m, idx] = max(abs(real(diag(Lambda))));
+fprintf(1, 'max eigenvalue = %g+%gi\n', real(m), imag(m));
 
-fprintf('max igenvalue %f\n', m);
 % Or find all, in case length(max(|lambda|)) > 1 (more than 1 max-s)
 % d = diag(abs(Lambda))
 % indices = find(max(d) == d)
