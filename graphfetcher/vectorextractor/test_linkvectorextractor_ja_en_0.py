dcf4d36aadcabb8edf26b2d2cda41a5e79ae6818
yamauchi.hitoshi <yamauchi@sunday.researcher>
2012-08-27 01:09:11 +0200

    Updated LineVectorExtractor to accept multiple input files. Now we can call get_link_list multiple times and then, cann export_to_file.

--- graphfetcher/vectorextractor/test_linkvectorextractor_ja_en_0.py
+++ graphfetcher/vectorextractor/test_linkvectorextractor_ja_en_0.py
@@ -3,11 +3,11 @@
 #
 # Copyright (C) 2012 Yamauchi, Hitoshi
 #
-# LinkVectorExtractor test: en_en (English Writer, English Wiki)
+# LinkVectorExtractor test: ja_en (Japanese Writer, English Wiki)
 #
 """
 \file
-\brief LinkVectorExtractor test: en_en (English Writer, English Author)
+\brief LinkVectorExtractor test: ja_en (Japanese Writer, English Wiki)
 """
 
 import os
@@ -18,28 +18,13 @@ class TestLinkVectorExtractor(unittest.TestCase):
     """test: LinkVectorExtractor test."""
 
     def test_linkvectorextractor(self):
-        """test generate URL list by List of English writers (en_en).
-        The export encoding option 'ascii' gives you matlab readable
-        author vector.
+        """test generate URL list by List of Japanese writers from English wiki (ja_en).
+        The export encoding option 'ascii' gives you matlab readable author vector.
         """
         # print u'# Need LC_ALL setting to utf-8, e.g., en_US.utf-8, ja_JP.utf-8.'
         graphfetcherdir = u'/home/hitoshi/data/project/shitohichi-tools/graphfetcher/'
 
-        input_rpath       = u'data/english_writer/en.wikipedia.org/wiki/'
-        author_root_fname = u'List_of_English_writers'
-
-        indir = os.path.join(graphfetcherdir, input_rpath)
-        input_fullpath = os.path.join(indir, author_root_fname)
-        root_url    = u'file:///' + input_fullpath
-
-        output_rpath      = u'data/english_writer/en.wikipedia.org/'
-        output_list_fname = u'en_en_writer.vector'
-        # output_list_fname = u'en_en_writer.ascii.vector'
-        outdir = os.path.join(graphfetcherdir, output_rpath)
-        output_full_path = os.path.join(outdir, output_list_fname)
-
-        print u'# input [' + root_url    + u']'
-        print u'# output[' + output_full_path + u']'
+        input_rpath       = u'data/japanese_writer/en.wikipedia.org/wiki/'
         ignore_href_list = [
             '../w/index.php', 'Category', '/wiki', 'Wikipedia:', 'Portal',
             "List_of_children's_literature_authors",
@@ -61,11 +46,31 @@ class TestLinkVectorExtractor(unittest.TestCase):
             '/w/index.php?title=Special:Cite&page=List_of_English_writers&id=494209347'
             ]
 
-        optdict = {'export_encoding': 'utf-8'}
-        # optdict = {'export_encoding': 'ascii'}
+        author_root_fname = u'List_of_Japanese_writers:_'
+        # List_of_Japanese_writers:_[A-Z], but some of them are not there, (e.g., X)
+        author_first_char_list = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+                                  'M', 'N', 'O', 'R', 'S', 'T', 'U', 'W', 'Y', 'Z' ]
+
+        output_rpath      = u'data/japanese_writer/en.wikipedia.org/'
+        outdir = os.path.join(graphfetcherdir, output_rpath)
+
+        output_list_fname = u'ja_en_writer.vector'
+        # output_list_fname = u'en_en_writer.ascii.vector'
+        output_full_path = os.path.join(outdir, output_list_fname)
+
+        # optdict = {'export_encoding': 'utf-8'}
+        optdict = {'export_encoding': 'ascii'}
         # optdict = {'export_encoding': 'shift-jis'}
+
         lf = LinkVectorExtractor.LinkVectorExtractor(ignore_href_list, optdict)
-        lf.get_link_list(root_url)
+
+        for prefix_ch in author_first_char_list:
+            indir = os.path.join(graphfetcherdir, input_rpath)
+            input_fullpath = os.path.join(indir, author_root_fname + prefix_ch)
+            root_url    = u'file:///' + input_fullpath
+
+            print u'# input [' + root_url    + u']'
+            lf.get_link_list(root_url)
 
         print u'# export [' + output_full_path + u']'
         lf.export_to_file(output_full_path)
