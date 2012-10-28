b99865224c7c97dacc29270fdeec90b71adda682
hyamauchi <hyamauchi@hyamauchi-lt2.nvidia.com>
2012-10-28 20:36:06 +0100

    Fixed read_authorvector when the comment line is in the middle of the file. Added test for that.

--- graphfetcher/pagerank/test_read_authorvector.m
+++ graphfetcher/pagerank/test_read_authorvector.m
@@ -2,6 +2,7 @@ function [] = test_read_authorvector()
 %TEST_READ_AUTHORVECTOR test for author vector read to a cell array
 %
 % 
-
-lines = read_authorvector('../vectorextractor/baseline/en_en_writer.ascii.vector');
+clear
+% lines = read_authorvector('../vectorextractor/baseline/en_en_writer.ascii.vector');
+lines = read_authorvector('../vectorextractor/baseline/en_en_writer_1.ascii.vector');
 disp(lines)
